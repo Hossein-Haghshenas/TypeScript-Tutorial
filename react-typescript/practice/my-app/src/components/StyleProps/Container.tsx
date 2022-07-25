0f2359b1a6ef70b3a7ec04db99a654cafebdb24c
Hossein <77438070+Hossein-Haghshenas@users.noreply.github.com>
2022-07-25 18:43:05 +0430

    Learn style props

---
+++ react-typescript/practice/my-app/src/components/StyleProps/Container.tsx
@@ -0,0 +1,7 @@
+import { ContainerProps } from "./Container.types";
+
+function Container({ styles }: ContainerProps) {
+  return <div style={styles}>text content goes here</div>;
+}
+
+export default Container;
